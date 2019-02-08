25e20c47f9e6a1d44d45a54e7e2f4e13be52513a
Gitte Sevel Bilenberg <gitt7083@edu.easj.dk>
2019-02-08 13:50:03 +0100

    Opgave 1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Studenter
+{
+    public Student()
+    {
+        {
+            private string _navn;
+            private int _fødselsmåned;
+            private int _fødselsdag;
+
+        }
+    
+
+        
+         public Studenter (string navn, int fødselsmåned, int fødselsdag)
+        {
+        _navn = navn;
+        _fødselsmåned = fødselsmåned;
+        _fødselsdag = fødselsdag;
+        }
+        public string Navn
+        {
+        get { return _navn; }
+        }
+        
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+
+        }
+
+         public int Fødselsmåned
+        {  
+                get { return _fødselsmåned; }
+
+        }
+public void PrintInformation(string header)
+        {
+        Console.WriteLine(header);
+        Console.WriteLine($"{Navn} er født {Fødselsdag} i {Fødselsmåned");
+
+        }
+    }
+}
